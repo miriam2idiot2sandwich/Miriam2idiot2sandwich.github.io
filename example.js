14499c74cc698eba91371d425175aebaa72713ab
Miriam2idiot2sandwich <miriam19ali@gmail.com>
2017-12-12 16:29:37 +0000

    new filling-in and colour picker

--- example.js
+++ example.js
@@ -1,16 +1,33 @@
-var name = "Miriam";
-
-console.log(name);
-document.querySelector("#myname").innerHTML = "Helo My name is " + name;
+function multiply(x, y) {
+return x * y;
+}
 
 var button = document.querySelector("#calculate")
 button.onclick = function() {
  var input1 = document.querySelector("#first").value;
  var input2 = document.querySelector("#second").value;
 
-var result = input1 * input2
+var result = multiply(input1, input2);
 
 console.log(result);
 document.querySelector("#answer").innerHTML = result;
 
+}
+
+function updateName(newName) {
+	if (newName === "Miriam") {
+		document.querySelector("#myname").innerHTML = "Hey, my name is Miriam too!";
+	} else {
+	document.querySelector("#myname").innerHTML = "Hello, " + newName
+   }
+}
+
+var namefield = document.querySelector("#nameinput");
+namefield.oninput = function () {
+updateName(namefield.value);
+
+
+var colourfield = document.querySelector("#colour");
+colourfield.oninput = function() {
+document.querySelector("#colourchange").style.color = colourfield.value;
 }
