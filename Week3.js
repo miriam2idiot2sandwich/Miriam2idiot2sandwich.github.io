4eecf92e0e8392fab540b0a431622d1031a091a1
Miriam2idiot2sandwich <miriam19ali@gmail.com>
2018-01-09 15:01:14 +0000

    Add loops

--- Week3.js
+++ Week3.js
@@ -8,16 +8,30 @@ if (1+1===3) {
 	alert("Oh dear");
 }
 
+for (i = 8; i <= 43; i+=2) {
+    console.log(i);
+}
+
 function checkPassword() {
     console.log("Yes, that's a password");
     var password = document.getElementById("password").value;
     alert(password);
-if (password === "swordfish") {
+	if (password === "swordfish") {
         console.log(password + " is correct!");
     } else if (password==="Miriam") {
 
-    	
+
     } else {
     	console.log("Not right");
+    }
+}
+
+function duplicate() {
+    var image = document.getElementById("target").innerHTML;
+    var copies = document.getElementById("copies").value;
+    document.getElementById("result").innerHTML = "";
+
+    for (i = 1; i <= copies; i++) {
+        document.getElementById("result").innerHTML += image;
     }
 }
